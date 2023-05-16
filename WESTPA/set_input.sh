3e2bfa7b3879f57acdbabaa424af5f3fe0a41b2b
Albert2424 <albertixortega@gmail.com>
2023-05-16 13:06:24 +0200

    set_input for MAB

--- WESTPA/set_input.sh
+++ WESTPA/set_input.sh
@@ -8,9 +8,9 @@ sed -i '9s/.*/'"$(sed -n '7p' input.dat)"'/' westpa_scripts/runseg.sh
 sed -i '10s/.*/'"$(sed -n '8p' input.dat)"'/' westpa_scripts/runseg.sh
 sed -i '11s/.*/'"$(sed -n '9p' input.dat)"'/' westpa_scripts/runseg.sh
 
-sed -i '25s/.*/'"$(sed -n '14p' input.dat)"'/' west.cfg
-sed -i '26s/.*/'"$(sed -n '16p' input.dat)"'/' west.cfg
-sed -i '30s/.*/'"$(sed -n '18p' input.dat)"'/' west.cfg
-#sed -i '23s/.*/'"$(sed -n '20p' input.dat)"'/' west.cfg
+sed -i '31s/.*/'"$(sed -n '14p' input.dat)"'/' west.cfg
+sed -i '32s/.*/'"$(sed -n '16p' input.dat)"'/' west.cfg
+sed -i '36s/.*/'"$(sed -n '18p' input.dat)"'/' west.cfg
+sed -i '23s/.*/'"$(sed -n '20p' input.dat)"'/' west.cfg
 
 sed -i '9s/.*/'"$(sed -n '24p' input.dat)"'/' runwe.slurm
