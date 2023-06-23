621e51bbc358e520cab3b7631b685491f9295995
Albert2424 <albertixortega@gmail.com>
2023-06-23 11:27:18 +0200

    print clusters on logs

--- WESTPA/WESTPA_GPU/westpa_scripts/runseg.sh
+++ WESTPA/WESTPA_GPU/westpa_scripts/runseg.sh
@@ -5,15 +5,15 @@
 seq='WT'       #name of the studied protein (str)
 temp=320       #temperature (int)
 cutoff=4.0     #cutoff (float)
-tau=600000     #steps of the simulation (tau in westpa) (int)
+tau=250000     #steps of the simulation (tau in westpa) (int)
 n_chains=100   #number of chains of the system (int)
 L=300.         #length of the box
-rc=30.         #minimum radius to consider a chain is inside a cluster
+rc=17.         #minimum radius to consider a chain is inside a cluster
 
-if [ -n "$SEG_DEBUG" ] ; then
-  set -x
-  env | sort
-fi
+#if [ -n "$SEG_DEBUG" ] ; then
+#  set -x
+#  env | sort
+#fi
 
 cd $WEST_SIM_ROOT
 mkdir -pv $WEST_CURRENT_SEG_DATA_REF
@@ -37,14 +37,20 @@ elif [ "$WEST_CURRENT_SEG_INITPOINT_TYPE" = "SEG_INITPOINT_NEWTRAJ" ]; then
   ln -sv $WEST_PARENT_DATA_REF/top.pdb ./parent.pdb
 fi
 
-CUDA_VISIBLE_DEVICES=$WM_PROCESS_INDEX
+export CUDA_DEVICES=(`echo $CUDA_VISIBLE_DEVICES_ALLOCATED | tr , ' '`)
+export CUDA_VISIBLE_DEVICES=${CUDA_DEVICES[$WM_PROCESS_INDEX]}
+
+echo "RUNSEG.SH: CUDA_VISIBLE_DEVICES_ALLOCATED = " $CUDA_VISIBLE_DEVICES_ALLOCATED
+echo "RUNSEG.SH: WM_PROCESS_INDEX = " $WM_PROCESS_INDEX
+echo "RUNSEG.SH: CUDA_VISIBLE_DEVICES = " $CUDA_VISIBLE_DEVICES
+echo "node name" $SLURMD_NODENAME
 
 # Run the dynamics with OpenMM
 python simulate.py --name $seq --temp $temp --cutoff $cutoff --steps $tau --n_chains $n_chains --L $L
 
 #Calculate pcoord with MDAnalysis
 python analyse.py #initialize the functions on analyse to be used in cluster.py
-python cluster.py --seq $seq --rc $rc --L $L --n_chains $n_chains > clust.log
+python cluster.py --seq $seq --rc $rc --L $L --n_chains $n_chains
 cat dist.dat > $WEST_PCOORD_RETURN
 
 # Clean up
