c3fecb4e560b2934521f495c04793a58d47f9c09
Albert2424 <albertixortega@gmail.com>
2023-07-05 12:48:45 +0200

    adapted to work with n>100

--- WESTPA/extra/merge_traj.py
+++ WESTPA/extra/merge_traj.py
@@ -12,6 +12,12 @@ import mdtraj as md
 import re
 import numpy as np
 
+'''
+REQUIRES:
+    - seg_logs of the <iteration> (ex: segs_logs)
+    - seg_traj/<iteration> (ex: 000081)
+'''
+
 #Sets the order in which the files must be read.
 
 sizes = [] 
@@ -19,24 +25,22 @@ for root, dirs, files in os.walk('/home/albert/TFM/RESULTS_ft3/seg_logs'):
 
     for file in files: 
         aux = []
-        with open('/home/albert/TFM/RESULTS_ft3/seg_logs/'+file, "r") as f:
+        with open('seg_logs/'+file, "r") as f:
             for line in f:
                 s = line.split()
                 if line.startswith('cluster'):
                     aux.append(int(s[3]))
-        sizes.append(f'{np.max(aux):02d}')
+        sizes.append(f'{np.max(aux):03d}')
                     
     arr = np.vstack((files,sizes))   
     arr = arr[:,arr[1,:].argsort()] 
-    
-    
 #generate the merged dcd
-
 segs = len(files)
 
 count = 1
 for i in arr[0]:
-    directory = i.split('-')[-1][:6]
+    directory = i.split('-')[0]+'/'+i.split('-')[-1][:6]
+    # print(directory)
     print(count,f' / {segs:}')
     
     if list(arr[0]).index(i) == 0:
